374b09a130fe937a3643749d89c6b82522769a9a
Fahad Kassim <fahad@yoteyote.com>
2015-02-11 00:38:10 +0200

    Footer changed :baby:

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,7 +1,18 @@
 </div> <!-- WRAPPER -->
 
 <footer id="footer">
-	<div class="disclaimer">Footer Here</div>
+<div class="container" id='footer-top-credit'>
+	<img class="img-responsive" src="<?php echo base_url().'assets/img/logo.png';?>"/>
+	<p class="text-left">Raudhwa Islamic Foundation</p>
+	</div>
+	
+	<div class="disclaimer">
+		<div class="container" id="footer-menu">
+		Fiqh | Sira
+		</div>
+	</div>
+	
+	
 </footer>
 
 <script src="<?php echo base_url()?>assets/js/jquery-1.11.1.js"></script>
--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title><?php echo $app_title?> | <?php if(isset($title) and $title){echo $title;}else{ echo "Karibu";}?></title>
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,800' rel='stylesheet' type='text/css'>
-    <link  href="<?php echo base_url()?>assets/css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link  href="<?php echo base_url()?>assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
     <link  href="<?php echo base_url()?>assets/css/font-awesome.css" rel="stylesheet" type="text/css">
     <link  href="<?php echo base_url()?>assets/css/bootstrap-social.css" rel="stylesheet" type="text/css">
     <link  href="<?php echo base_url()?>assets/css/style.css" rel="stylesheet" type="text/css">
