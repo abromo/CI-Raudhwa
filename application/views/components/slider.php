e085b09a6cb631f63d1b06459e09fa52173efc19
Fahad Kassim <fahad@yoteyote.com>
2015-02-13 02:07:48 +0200

    Changed Slogan to Static TEXT :nail_care:

--- application/views/components/slider.php
+++ application/views/components/slider.php
@@ -6,7 +6,7 @@
                 <div class="row">
                     <div id = "content-title" class="pull-right col-xs-6">
                         <div class="row"><h1><?php echo $slider_title;?></h1></div>
-                        <div class="row"><h5><?php echo $slider_slogan?></h5></div>
+                        <div class="row kidole"><h5 style="padding-right: 3px;">SOMA ZAIDI <i class="fa fa-chevron-right"></i></h5></div>
 
                     </div>
                 </div>
