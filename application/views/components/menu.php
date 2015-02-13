696189f7015451a3a4235a90779f03152deef3db
Fahad Kassim <fahad@yoteyote.com>
2015-02-13 02:08:06 +0200

    Added Real Content to the MENU :necktie:

--- application/views/components/menu.php
+++ application/views/components/menu.php
@@ -13,29 +13,39 @@
             <div class="container">
                 <div class="row">
                     <div id="logo">
-                        <img src="<?php echo base_url()?>assets/img/logo.png" alt=""/><span class="brand" style="line-height: 0">Raudhwa</span>
+                        <a href="<?php echo base_url(); ?>"><img src="<?php echo base_url()?>assets/img/logo.png" alt=""/><span class="brand" style="line-height: 0">Raudhwa</span></a>
                     </div>
                     <div id='cssmenu'>
                         <ul>
-                            <li><a href='#'><span>Home</span></a></li>
-                            <li class='active has-sub'><a href='#'><span>Products</span></a>
+                            <li><a href='#'><span>Masuala na Majibu</span></a></li>
+                            <li class='active has-sub'><a href='#'><span>Sikiliza</span></a>
                                 <ul>
-                                    <li class='has-sub'><a href='#'><span>Product 1</span></a>
+                                    <li class='has-sub'><a href='#'><span>Quraan</span></a>
                                         <ul>
-                                            <li><a href='#'><span>Sub Product</span></a></li>
-                                            <li class='last'><a href='#'><span>Sub Product</span></a></li>
+                                            <li><a href='#'><span>Tafsiri</span></a></li>
+                                            <li class='last'><a href='#'><span>Qiraat</span></a></li>
                                         </ul>
                                     </li>
-                                    <li class='has-sub'><a href='#'><span>Product 2</span></a>
+                                    <li class='has-sub'><a href='#'><span>Sira</span></a>
                                         <ul>
-                                            <li><a href='#'><span>Sub Product</span></a></li>
-                                            <li class='last'><a href='#'><span>Sub Product</span></a></li>
+                                            <li><a href='#'><span>Mitume</span></a></li>
+                                            <li><a href='#'><span>Maswahaba</span></a></li>
+                                            <li class='last'><a href='#'><span>Matabiina</span></a></li>
                                         </ul>
                                     </li>
                                 </ul>
                             </li>
-                            <li><a href='#'><span>About</span></a></li>
-                            <li class='last'><a href='#'><span>Contact</span></a></li>
+                            <li class='has-sub'><a href='#'><span>Vitabu</span></a>
+                                <ul>
+                                    <li class='has-sub'><a href='#'><span>Fiqhi</span></a></li>
+                                    <li class='has-sub'><a href='#'><span>Sira</span></a></li>
+                                    <li class="has-sub"><a href=""><span>Hadeeth</span></a></li>
+                                    <li class="has-sub"><a href=""><span>Vengine</span></a></li>
+                                </ul>
+                            </li>
+                            <li><a href='#'><span>Video</span></a></li>
+                            <li><a href='#'><span>Kuhusu sisi</span></a></li>
+                            <li class='last'><a href='#'><span>Wasiliana nasi</span></a></li>
                         </ul>
                     </div>
                 </div>
