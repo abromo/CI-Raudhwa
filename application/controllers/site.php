2462b6a96f1775b996fb78a247314406e6d33f4b
Fahad Kassim <fahad@yoteyote.com>
2015-02-20 23:07:20 +0200

    Controller for Site is ready :ice_cream:

--- application/controllers/site.php
+++ application/controllers/site.php
@@ -14,9 +14,11 @@ class Site extends CI_Controller {
 
 	public function index()
 	{
-        $data['title'] = "Mwanzo";
+        $data['title'] = "Makala";
         $data['active'] = "index";
-        $data['slider_title'] = "Ijue dini yako";
+        $random_article = $this->posts->get_random_article();;
+        $data['slider_title'] = $random_article['title'];
+        $data['slider_id'] = $random_article['post_id'];
         $data['slider_slogan'] = "Know Your Deen";
 
         $data['posts'] = $this->posts->get_latest_posts(); //from DB
@@ -29,6 +31,17 @@ class Site extends CI_Controller {
 		$this->load->view('pages/index');
         $this->load->view('includes/footer');
 	}
+
+    function  get_post(){
+        if(IS_AJAX){
+            $id = $this->input->post('id');
+            $post = $this->posts->get_post($id);
+            echo json_encode($post);
+
+        }else{
+            echo "Direct Access Is forbidden ;)";
+        }
+    }
 
     public function fiqhi()
 	{
--- application/models/posts.php
+++ application/models/posts.php
@@ -1,8 +1,22 @@
 <?php
  class Posts extends  CI_Model{
      function get_latest_posts (){
-         $q = "SELECT * FROM POSTS P,ARTICLES A WHERE P.post_id=A.post_id";
+         $q = "SELECT * FROM POSTS P,ARTICLES A WHERE P.post_id=A.post_id ORDER BY P.created_date DESC";
          $posts = $this->db->query($q);
          return $posts->result_array();
+     }
+
+     function get_random_article(){
+         $q = "SELECT * FROM POSTS P,ARTICLES A WHERE P.post_id = A.post_id ORDER BY RAND() limit 1";
+         $posts = $this->db->query($q);
+         $post = $posts->result_array();
+         return $post[0];
+     }
+
+     function get_post($id){
+         $q = "SELECT * FROM POSTS P,ARTICLES A WHERE P.post_id='$id' AND A.post_id ='$id'";
+         $posts = $this->db->query($q);
+         $post = $posts->result_array();
+         return $post[0];
      }
  }
