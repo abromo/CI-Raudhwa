3003d375a93bc59b289dd03a7cd222bb37ccc4d6
Fahad Kassim <fahad@yoteyote.com>
2015-02-20 23:06:52 +0200

    Slider is now Dynamic :vibration_mode:

--- application/views/components/slider.php
+++ application/views/components/slider.php
@@ -1,12 +1,12 @@
 
 <div class="row" id="slider">
     <div>
-        <div class="row img-slider">
+        <div class="row img-slider" style="background: url('<?php echo base_url('assets')?>/img/cami.png') no-repeat center;-webkit-background-size: cover;-moz-background-size: cover;-o-background-size: cover;background-size: cover;">
             <div class="container">
                 <div class="row">
-                    <div id = "content-title" class="pull-right col-xs-6">
-                        <div class="row"><h1><?php echo $slider_title;?></h1></div>
-                        <div class="row kidole"><h5 style="padding-right: 3px;">SOMA ZAIDI <i class="fa fa-chevron-right"></i></h5></div>
+                    <div id="content-title" class="pull-right col-xs-10">
+                        <div class="row"><h1><?php echo character_limiter($slider_title,21,"");?></h1></div>
+                        <div class="row kidole"><h5 class="soma-zaidi" style="padding-right: 3px;" data-id="<?php echo $slider_id ; ?>">SOMA ZAIDI <i class="fa fa-chevron-right"></i></h5></div>
 
                     </div>
                 </div>
