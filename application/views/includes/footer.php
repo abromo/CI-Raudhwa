4bda152ec1f4b8b8544799cae013cac76bc9708b
Fahad Kassim <fahad@yoteyote.com>
2015-02-20 23:06:39 +0200

    Welcome moment JS :cactus:

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -20,8 +20,9 @@
 </footer>
 
 <script src="<?php echo base_url()?>assets/js/jquery-1.11.1.js"></script>
-<!--    <script src="js/menubar.js"></script>-->
-<script src="<?php echo base_url()?>assets/js/menuAnim.js"></script>
+<script src="<?php echo base_url()?>assets/js/moment.js"></script>
+<script src="<?php echo base_url()?>assets/js/rau.js"></script>
+
 </body>
 
 </html>
